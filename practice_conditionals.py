6da7d628d0047a294f0f0447983f4d4fb4810292
Steven Tran <steventran@asu.edu>
2021-03-23 23:11:46 -0700

    some good old practice with conditionals

---
+++ practice_conditionals.py
@@ -0,0 +1,19 @@
+# the exercise in conditionals
+# by @stevemeisterr
+# 
+# 2021 - 03 - 23 #
+
+print('Hi, how the heck are ya?')
+response = input('Respond...')
+
+if response == "I'm great!":
+    greeting = "Well, fuck you then! "
+elif response == "I'm grreat!":
+    greeting = "Well if it isn't Tony the FUCKING Tiger.. "
+else:
+    greeting = "Well if you're not having a good day, I hope it get's better soon. "
+
+greeting = greeting + '\n'
+message = greeting + 'Anyway... \n' + 'Have a good one!'
+
+print(message)

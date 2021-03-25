7c9fef34b42c7019806f83a1a98539faee437d09
Steven Tran <steventran@asu.edu>
2021-03-24 19:26:23 -0700

    nothing important

--- practice_conditionals.py
+++ practice_conditionals.py
@@ -4,11 +4,11 @@
 # 2021 - 03 - 23 #
 
 print('Hi, how the heck are ya?')
-response = input('Respond...')
+response = input('Provide your response: ')
 
 if response.startswith("I'm great"):
     greeting = "Well, fuck you then! "
-elif response == "I'm grreat!":
+elif response.startswith("I'm grreat"):
     greeting = "Well if it isn't Tony the FUCKING Tiger.. "
 else:
     greeting = "Well if you're not having a good day, I hope it get's better soon. "
